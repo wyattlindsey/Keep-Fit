b512c3331c2aa6001872c08fad2f776b5f1bc023
Internev <nchallinger@gmail.com>
2016-12-10 10:59:29 +1100

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -7,7 +7,7 @@ module.exports = function (app, express) {
 
   // app.use(morgan()); <-- debugging
   //app.use(bodyParser.urlencoded()); <-- use for ajax post requests
-  app.use(express.static(__dirname + './client'));
+  app.use(express.static(__dirname + '../client'));
   //app.get('/',requestHandler.getIndex);
   app.post('/api/addUser',requestHandler.addUser);
   app.get('/api/logIn',requestHandler.logIn);
