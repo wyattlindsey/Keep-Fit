4af704cb3fe735a930c107fe9bf0e128c43f41dc
Neville Neville <nchallinger@gmail.com>
2016-12-13 14:48:06 +1100

    Add post request through express for workouts

--- server/routes.js
+++ server/routes.js
@@ -6,15 +6,15 @@ var path = require('path');
 
 module.exports = function (app, express) {
 
-  //app.use(morgan()); //<-- debugging
+  // app.use(morgan()); //<-- debugging
   //app.use(bodyParser.urlencoded()); <-- use for ajax post requests
   app.use(express.static(path.join(__dirname, '../client')));
   //app.get('/',requestHandler.getIndex);
   app.get('/api/getUser',requestHandler.getUser);
-  app.get('/api/signUp',requestHandler.signUp);
-  app.get('/api/submitWorkout', requestHandler.submitWorkout);
+  app.post('/api/signUp',requestHandler.signUp);
+  app.post('/api/submitWorkout', requestHandler.submitWorkout);
   app.get('/api/getWorkouts', requestHandler.getWorkouts);
-  app.get('/api/submitExercise', requestHandler.submitExercise);
+  app.post('/api/submitExercise', requestHandler.submitExercise);
   app.get('/api/getExercises', requestHandler.getExercises);
 
 
