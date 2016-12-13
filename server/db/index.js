292dcb6a9ffd682e0212518541eefa7246aa6e54
Andrew Chung <chung.andrew7@gmail.com>
2016-12-13 00:03:49 -0500

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   // port: '8000',
   user: 'root',
-  password: 'chooChung89',
+  password: '',
   database: 'keepFit'
 });
 
