02d19095558b684af683e38bb8f7ef48402b14b7
Wyatt Lindsey <wyatt.b.lindsey@gmail.com>
2016-12-15 15:55:16 -0800

    install mongoose, configure and import database into running Node server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,11 @@
-var express = require('express');
-var helper = require('./helper.js');
-var app = express();
+const express = require('express');
+const helper = require('./helper.js');
+const app = express();
+const config = require('./config');
+const routes = require('./routes.js')(app, express);
 
-require('./routes.js')(app, express);
-// start listening to requests on port 8000
-app.listen(process.env.PORT || 8000);
-console.log('Server up and ready to serve!');
-// app.listen(8000, function(err) {
-//   if (err) {
-//       console.log('Error', err);
-//   } else {
-//       console.log('Server listening on', 8000);
-//   }
-// });
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server up and ready to serve!');
+});
 
-// export our app for testing and flexibility, required by index.js
 module.exports.app = app;
