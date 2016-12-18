036166d8d01611e1e5d42af5aecaccfb3bd16ae2
Luke Golden <webseedsonline@gmail.com>
2016-12-17 16:35:31 -0800

    Completed basic dash view and added styling

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,36 +1,87 @@
 import {Link} from 'react-router';
-import Home from './Home.jsx';
+import Nav from './Nav.jsx';
 
 
 export default class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      weightLifting: [],
+      running: []
     };
+    this.getUsersWorkouts = this.getUsersWorkouts.bind(this);
   }
 
+  componentDidMount() {
+    this.getUsersWorkouts();
+  }
+
+  getUsersWorkouts() {
+    var that = this;
+    $.get(`/users/${userId}/workouts`, function(data) {
+      if(data) {
+        console.log('success!', data);
+        data.forEach(function(workout) {
+          var parsedExercises = JSON.parse(workout.exercises);
+          workout.exercises = parsedExercises;
+          if(workout.type === 'weight-lifting') {
+            var weightLifting = that.state.weightLifting.slice();
+            weightLifting.push(workout);
+            that.setState({ weightLifting: weightLifting});
+          }
+        });
+      }
+    });
+  }
 
   render() {
     return (
       <div>
-        {<Home />}
-        <div className="container">
-          
-          <div className="col-md-4">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-                <h3 class="panel-title">
-                  Panel title
-                </h3>
-              </div>
-              <div class="panel-body">
-                Panel content
-              </div>
-              <div class="panel-footer">
-                Panel footer
-              </div>
-            </div>
+        {<Nav />}
+        <div className="container-fluid">
+
+          <div className="row">
+            {this.state.weightLifting.map( (workout, index) => {
+
+              return (
+                  <div className="col-md-4">
+                    <div className="panel panel-default">
+                      <div className="panel-heading">
+                        <h3 className="panel-title">
+                          {workout.name}
+                        </h3>
+                      </div>
+                      <div className="panel-body">
+                        <table>
+                          <thead>
+                            <tr>
+                              <td>Exercise</td>
+                              <td>Weight</td>
+                              <td>Reps</td>
+                            </tr>
+                          </thead>
+                          <tbody>
+                          {workout.exercises.map( (exercise, index) => {
+                            return (
+                              <tr key={index}>
+                                <td>{exercise.name}</td>
+                                <td>{exercise.weight}</td>
+                                <td>{exercise.reps}</td>
+                              </tr>
+                            )
+                          })}
+                          </tbody>
+                        </table>
+                      </div>
+                      <div className="panel-footer flex-center">
+                        <button type="button" className="btn btn-default">
+                          Complete Workout
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+              )
+            })}
           </div>
         </div>
       </div>
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,12 +3,18 @@
 import {Router, Route, IndexRoute, Link, browserHistory} from 'react-router';
 
 
-export default class Home extends React.Component {
+export default class Nav extends React.Component {
   constructor(props) {
     super(props);
     this.state= {
 
     }
+  }
+
+  reRouteAddWorkout() {
+    return (
+      <Link to="/lifting">Add Workout</Link>
+    )
   }
 
   render() {
@@ -22,7 +28,7 @@ export default class Home extends React.Component {
               {/* This is where we could add dynamic user login status
             <div className="navbar-right text-white">User</div>*/}
             <button type="button" className="btn btn-default btn-lg navbutton-right">
-              <Link to="/lifting">Add Workout</Link>
+              {this.reRouteAddWorkout()}
             </button>
           </nav>
         </div>
