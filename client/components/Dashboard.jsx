4f87c0bb1874899cb682e4d4de01edf59f766d7c
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2016-12-19 13:39:11 -0500

    Dio changes.

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router';
 import Nav from './Nav.jsx';
-
+import HeatMap from './HeatMap.jsx';
 
 export default class Dashboard extends React.Component {
   constructor(props) {
@@ -10,17 +10,30 @@ export default class Dashboard extends React.Component {
       running: []
     };
     this.getUsersWorkouts = this.getUsersWorkouts.bind(this);
+    this.completeWorkoutHandler = this.completeWorkoutHandler.bind(this);
   }
 
   componentDidMount() {
     this.getUsersWorkouts();
   }
 
+  completeWorkoutHandler() {
+    var didTheThing = {};
+    var userId = window.sessionStorage.user;
+    didTheThing.workedOut = true;
+    $.post(`api/users/${userId}/events`, didTheThing, function(resp) {
+      if(resp) {
+        console.log('Woohoo! You have completed your workout. Keep it up! ', resp);
+      } else {
+        console.log('Uh-Oh.. your workout cannot be completed at this time. Sorry! ', err);
+      }
+    });
+  }
+
   getUsersWorkouts() {
     var that = this;
     var userId = window.sessionStorage.user;
     $.get(`/api/users/${userId}/workouts`, function(data) {
-
       if(data) {
         console.log('success!', data);
         data.forEach(function(workout) {
@@ -41,7 +54,7 @@ export default class Dashboard extends React.Component {
       <div>
         {<Nav />}
         <div className="container-fluid">
-
+          {<HeatMap/>}
           <div className="row">
             {this.state.weightLifting.map( (workout, index) => {
 
@@ -76,7 +89,7 @@ export default class Dashboard extends React.Component {
                         </table>
                       </div>
                       <div className="panel-footer flex-center">
-                        <button type="button" className="btn btn-default">
+                        <button type="button" className="btn btn-default" onClick={this.completeWorkoutHandler}>
                           Complete Workout
                         </button>
                       </div>
