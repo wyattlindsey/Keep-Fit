bf284741f39db79b84ed8b99999414602723899e
David Flowers <dvdwdflwrs@gmail.com>
2016-12-19 10:55:17 -0600

    Integrate running routes map

---
+++ client/runningRoute.js
@@ -0,0 +1,9 @@
+var runningRoute = L.map('runningRoute').setView([29.7604, -95.3698], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/davflo-16/ciwvhkb9s00ff2pnx7nix4bx8/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGF2ZmxvLTE2IiwiYSI6ImNpd3ZoZG43aDAwcnYydHNhbHNsc2FyOG8ifQ.dNmw9UwoMTPzkUCNhwh2RA', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(runningRoute);
+
+L.Routing.control({
+
+}).addTo(runningRoute);
