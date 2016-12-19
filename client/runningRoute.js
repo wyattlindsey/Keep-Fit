6000d781f19aa1ce78cb06a50c4b99ceb979cd7f
David Flowers <dvdwdflwrs@gmail.com>
2016-12-19 16:02:48 -0600

    Add location services to running routes map.  Minor styling edits

--- client/runningRoute.js
+++ client/runningRoute.js
@@ -1,14 +1,15 @@
 var runningRoute = L.map('runningRoute').setView([29.7604, -95.3698], 13);
+runningRoute.locate({setView: true});
+
+
+L.control.locate().addTo(runningRoute);
+
 L.tileLayer('https://api.mapbox.com/styles/v1/davflo-16/ciwvhkb9s00ff2pnx7nix4bx8/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGF2ZmxvLTE2IiwiYSI6ImNpd3ZoZG43aDAwcnYydHNhbHNsc2FyOG8ifQ.dNmw9UwoMTPzkUCNhwh2RA', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
 }).addTo(runningRoute);
 
 var control = L.Routing.control({
-    waypoints: [
-        L.latLng(29.7604, -95.3698),
-        L.latLng(29.7604, -95.3700)
-    ],
     routeWhileDragging: true
 }).addTo(runningRoute);
 
